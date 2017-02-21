cf47a2f562e14f96ba81b91be898710494109c95
Genjo <demnart93@gmail.com>
2017-02-21 10:17:59 +0200

    Создали класс модели новостей который наследуеться от абстрактной модели, и в котором мы описали поля таблицы и имени класса

---
+++ model/News.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: genjo
+ * Date: 2/21/17
+ * Time: 10:12 AM
+ */
+class News extends AbstractModel
+{
+
+    protected static $table = 'news';
+    protected static $className = 'News';
+}
